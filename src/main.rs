5debd20b25e85c0004dde39bc944d33427c123ef
Knarkzel <knarkzel@gmail.com>
2023-01-15 00:01:45 +0100

    Added spinner

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::{anyhow, Result};
 use chrono::{DateTime, Utc};
 use clap::{Parser, Subcommand};
+use comfy_table::{modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, Cell, Table};
 use dialoguer::Input;
+use indicatif::ProgressBar;
 use platform_dirs::AppDirs;
 use serde::{Deserialize, Serialize};
+use std::{fmt::Display, path::PathBuf, time::Duration};
 use ureq::Response;
-use std::path::PathBuf;
-use comfy_table::{Table, presets::UTF8_FULL, modifiers::UTF8_ROUND_CORNERS};
 
 // Args
 #[derive(Parser)]
@@ -52,9 +53,11 @@ impl Config {
 // Helpers
 fn fetch(route: &str) -> Result<Response> {
     let Config { token } = Config::read()?;
-    Ok(ureq::get(&format!("https://uia.instructure.com/api/v1/{route}"))
-       .set("Authorization", &format!("Bearer {token}"))
-       .call()?)
+    Ok(
+        ureq::get(&format!("https://uia.instructure.com/api/v1/{route}"))
+            .set("Authorization", &format!("Bearer {token}"))
+            .call()?,
+    )
 }
 
 // Types
@@ -72,6 +75,21 @@ struct Assignment {
     description: Option<String>,
 }
 
+struct Link {
+    url: String,
+    text: String,
+}
+
+impl Display for Link {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "\u{1b}]8;;{}\u{1b}\\{}\u{1b}]8;;\u{1b}\\",
+            self.url, self.text
+        )
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
@@ -85,33 +103,40 @@ fn main() -> Result<()> {
             println!("Wrote settings to {}", Config::path()?.display());
         }
         Command::Assignments => {
+            let spinner = ProgressBar::new_spinner().with_message("Loading courses...");
+            spinner.enable_steady_tick(Duration::from_millis(10));
             let courses = fetch("users/self/favorites/courses")?.into_json::<Vec<Course>>()?;
+            spinner.finish_and_clear();
             for Course { id, name } in courses {
+                let spinner = ProgressBar::new_spinner().with_message(format!("Loading assignments for {name}..."));
+                spinner.enable_steady_tick(Duration::from_millis(10));
                 let route = format!("courses/{id}/assignments");
                 let assignments = fetch(&route)?.into_json::<Vec<Assignment>>()?;
+                spinner.finish_and_clear();
                 if assignments.len() == 0 {
                     continue;
                 }
+                let link = Link {
+                    url: format!("https://uia.instructure.com/courses/{id}/assignments"),
+                    text: name,
+                };
+                println!("{link}");
                 let mut table = Table::new();
                 table
                     .load_preset(UTF8_FULL)
                     .apply_modifier(UTF8_ROUND_CORNERS)
                     .set_header(vec![
-                        name.as_str(),
-                        "Date",
-                        "Days left",
+                        Cell::new("Name"),
+                        Cell::new("Date"),
+                        Cell::new("Days left"),
                     ]);
                 for Assignment { name, due_at, .. } in assignments {
                     let now = chrono::offset::Utc::now();
                     let date = due_at.date_naive();
                     let delta = (due_at - now).num_days();
                     if delta >= 0 {
-                        table.add_row(vec![
-                            name,
-                            format!("{date}"),
-                            format!("{delta}"),
-                        ]);
-                    } 
+                        table.add_row(vec![Cell::new(name), Cell::new(date), Cell::new(delta)]);
+                    }
                 }
                 println!("{table}");
             }
