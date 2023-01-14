604a07ae09bbfe442f91e4854a3cb0bf87dc55af
Knarkzel <knarkzel@gmail.com>
2023-01-14 17:31:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# canvas
+
+```
+$ cargo install --git https://github.com/knarkzel/canvas
+$ canvas
+Usage: canvas <COMMAND>
+
+Commands:
+  login        Authenticate and store token locally
+  assignments  Fetch assignments from Canvas
+  help         Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help  Print help
+```
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use anyhow::{anyhow, Result};
+use clap::{Parser, Subcommand};
+use dialoguer::Input;
+use platform_dirs::AppDirs;
+use serde::{Serialize, Deserialize};
+use std::{path::PathBuf, fs::File, io::Write};
+
+#[derive(Parser)]
+struct Args {
+    /// Subcommand to run
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Authenticate and store token locally
+    Login,
+    /// Fetch assignments from Canvas
+    Assignments,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    token: String,
+}
+
+fn config_path() -> Result<PathBuf> {
+    let app_dirs = AppDirs::new(Some("canvas"), false).ok_or(anyhow!("Config location is invalid"))?;
+    std::fs::create_dir_all(&app_dirs.config_dir)?;
+    Ok(app_dirs.config_dir.join("canvas.toml"))
+}
+
+fn read_config() -> Result<Config> {
+    let config_file = config_path()?;
+    let raw = std::fs::read_to_string(config_file)?;
+    Ok(toml::from_str(&raw)?)
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Login => {
+            open::that("https://uia.instructure.com/profile/settings#access_tokens_holder")?;
+            let token = Input::<String>::new()
+                .with_prompt("Token")
+                .interact_text()?;
+
+            // Create config file
+            let config_file = config_path()?;
+            let message = format!("Wrote settings to {}", config_file.display());
+
+            // Serialize to toml
+            let mut file = File::create(config_file)?;
+            let config = Config { token };
+            let toml = toml::to_string(&config)?;
+
+            // Write to file
+            file.write_all(toml.as_bytes())?;
+            println!("{message}");
+        }
+        Command::Assignments => {
+            let Config { token } = read_config()?;
+            todo!()
+        },
+    }
+
+    Ok(())
+}
